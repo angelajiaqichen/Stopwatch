95bb7eba6f51a0fc4e27c4cc6d299d330199b610
Angela Chen <angelajiaqichen@gmail.com>
2022-09-07 18:43:29 -0400

    built stopwatch

---
+++ Stopwatch/Stopwatch.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+class Stopwatch {
+  
+  private var startTime: NSDate?
+  var isRunning:Bool = false
+
+  func start() {
+    startTime = NSDate()
+  }
+
+  func stop() {
+    startTime = nil
+  }
+  
+  var elapsedTime: TimeInterval {
+    if let startTime = self.startTime {
+      return -1 * startTime.timeIntervalSinceNow // could also just say -startTime.timeIntervalSinceNow
+    } else {
+      return 0
+    }
+  }
+  
+  func elapsedTimeAsString() -> String {
+    // return the formatted string...
+    let diffMinutes = Int(elapsedTime / 60)
+    let diffSeconds = Double(elapsedTime) - Double(diffMinutes * 60)
+    let displayedTime = String(diffMinutes) + ":" + String(format: "%.1f", -diffSeconds)
+    return displayedTime
+  }
+  
+
+   
+}
